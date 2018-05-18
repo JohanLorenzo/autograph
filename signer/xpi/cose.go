5a9255d17be92efcda8f3842a0d3e1ce71cc1623
Greg Guthe <gguthe@mozilla.com>
2018-05-18 11:50:08 -0400

    xpi: add COSE helper fns

---
+++ signer/xpi/cose.go
@@ -0,0 +1,58 @@
+package xpi // import "go.mozilla.org/autograph/signer/xpi"
+
+import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/rsa"
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
+	"go.mozilla.org/cose"
+)
+
+// stringToCOSEAlg returns the cose.Algorithm for a string or nil if
+// the algorithm isn't implemented
+func stringToCOSEAlg(s string) (v *cose.Algorithm) {
+	switch strings.ToUpper(s) {
+	case "PS256":
+		v = cose.PS256
+	case "ES256":
+		v = cose.ES256
+	case "ES384":
+		v = cose.ES384
+	case "ES512":
+		v = cose.ES512
+	default:
+		v = nil
+	}
+	return v
+}
+
+// generateIssuerEEKeyPair returns a public and private key pair for
+// the provided COSEAlgorithm
+func (s *PKCS7Signer) generateCOSEKeyPair(coseAlg *cose.Algorithm) (eeKey crypto.PrivateKey, eePublicKey crypto.PublicKey, err error) {
+	var signer *cose.Signer
+
+	if coseAlg == nil {
+		err = fmt.Errorf("Cannot generate private key for nil cose Algorithm")
+		return
+	} else if coseAlg == cose.PS256 {
+		const size = 2048
+		eeKey, err = s.getRsaKey(size)
+		if err != nil {
+			err = errors.Wrapf(err, "failed to generate rsa private key of size %d", size)
+			return
+		}
+		eePublicKey = eeKey.(*rsa.PrivateKey).Public()
+	} else {
+		signer, err = cose.NewSigner(coseAlg, nil)
+		if err != nil {
+			err = errors.Wrapf(err, "failed to generate private key")
+			return
+		}
+		eeKey = signer.PrivateKey
+		eePublicKey = eeKey.(*ecdsa.PrivateKey).Public()
+	}
+	return
+}
